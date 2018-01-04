1fdbb2f5f6e9f84a83dd9390603636f46e412533
colton <colton@Gunnars-MacBook-Pro.local>
2018-01-04 15:33:55 -0700

    updated

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -x PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 # go
 set -x GOROOT /usr/local/go
 set -x GOPATH ~/go_workspace $GOPATH
-set -x PATH $GOPATH/bin $PATH
+set -x PATH $GOPATH/bin $GOROOT/bin $PATH
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/coltonmorris/go_workspace/src/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/coltonmorris/go_workspace/src/google-cloud-sdk/path.fish.inc'; else; . '/Users/coltonmorris/go_workspace/src/google-cloud-sdk/path.fish.inc'; end; end
